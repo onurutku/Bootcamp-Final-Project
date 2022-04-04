39e2e1893d6dda19749f65caf9ee6f1ea48dbc53
onurutku <topaloglu08@gmail.com>
2022-04-04 23:24:39 +0300

    :bug: edit button bug fixed
    edit button bug fixed

--- src/app/shared/guards/deact.guard.ts
+++ src/app/shared/guards/deact.guard.ts
@@ -24,15 +24,21 @@ export class DeactGuard implements CanDeactivate<unknown> {
     | Promise<boolean | UrlTree>
     | boolean
     | UrlTree {
-    component.showPopUp = true;
     if (component.canILeave || !component.productLoadForm.dirty) {
+      // console.log(this.product.canILeave);
+      // console.log(this.product.canILeave.pipe(map(() => {})));
+      // console.log(this.product.canILeave.subscribe(() => {}));
+      // console.log(this.product.canILeave.observed);
       return true;
     }
+    component.showPopUp = true;
     //this subject wait as AnonymousSubject and when i click yes or cancel button it becomes
     //a observable and return my value. If use subscribe method instead of pipe(map). method return SafeSubscription
     //and i can't handle type Subscription in here. :S
     return this.product.canILeave.pipe(
+      //canILeave is a subject in service
       map((data) => {
+        //data return true or false
         component.showPopUp = false;
         return data;
       })
