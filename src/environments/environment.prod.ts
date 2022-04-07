1fe9048e99455909bd5fdbb9ac31a53a18816220
onurutku <topaloglu08@gmail.com>
2022-04-07 15:25:11 +0300

    :bug: responsive bug fixed
    responsive bug fixed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,14 +1,15 @@
 export const environment = {
   production: true,
   firebaseConfig: {
-    apiKey: 'AIzaSyC1IBUpE6IQdp36Ann5eMYUqsH4WMY-Sh0',
-    authDomain: 'angular-bootcamp-out.firebaseapp.com',
+    apiKey: "AIzaSyC1IBUpE6IQdp36Ann5eMYUqsH4WMY-Sh0",
+    authDomain: "angular-bootcamp-out.firebaseapp.com",
     databaseURL:
-      'https://angular-bootcamp-out-default-rtdb.europe-west1.firebasedatabase.app',
-    projectId: 'angular-bootcamp-out',
-    storageBucket: 'angular-bootcamp-out.appspot.com',
-    messagingSenderId: '295275887751',
-    appId: '1:295275887751:web:165a8dcda0f9ebe3e08c22',
-    measurementId: 'G-T2NM7JH9JB',
+      "https://angular-bootcamp-out-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "angular-bootcamp-out",
+    storageBucket: "angular-bootcamp-out.appspot.com",
+    messagingSenderId: "295275887751",
+    appId: "1:295275887751:web:165a8dcda0f9ebe3e08c22",
+    measurementId: "G-T2NM7JH9JB",
   },
 };
+export const _admin = "orionbootcamp@gmail.com"; //admin email address
